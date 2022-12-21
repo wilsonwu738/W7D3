f6e6d333548689a308bd2ac322bf63c2f0c312af
Wilson Wu <wuyecheng738@gmail.com>
2022-12-21 16:47:54 -0500

    user model

--- goal_app/spec/models/user_spec.rb
+++ goal_app/spec/models/user_spec.rb
@@ -24,10 +24,43 @@ RSpec.describe User, type: :model do
       create(:user)
     end
 
-  it { should validate_uniqueness_of(:username)}
-  it { should validate_uniqueness_of(:session_token)}
+    it { should validate_uniqueness_of(:username)}
+    it { should validate_uniqueness_of(:session_token)}
   end
 
+  describe "is_password?" do
+    let!(:user) { create(:user) }
+    context "with a valid password" do 
+      it "should return true" do 
+        expect (user.is_password?("password")).to be true
+      end
+    end
+
+    context "with an invalid password" do 
+      it "should return false" do 
+        expect (user.is_password?("notfun")).to be false
+      end
+    end
+  end
+
+  describe "password hashing" do
+    it 'does not ave passwords to the database' do
+      FactoryBot.create(:user, username: 'Harry Potter')
+      user = User.find_by(username: "Harry Potter")
+      expect(user.password).not_to eq('password')
+    end
+
+    it 'hashes password using BCrypt' do
+      ecpect(BCrypt::Password).to receive(:create).with('abcdefgh')
+      FactoryBot.build(:user, password: 'abcdefgh')
+    end
+  end
+  
+
+
+
+
+  
   
 end
 
